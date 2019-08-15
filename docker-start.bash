e242dd606ad3190a23d9225f18fd961c643073e7
Vasyl Tarasov <vasiltarasov704@gmail.com>
2019-08-15 11:54:02 +0300

    add control commands

--- docker-start.bash
+++ docker-start.bash
@@ -2,10 +2,13 @@
 
 task=$1
 
-if [ "$task" = "run" ]
+if [ "$task" = "up" ]
  then
-    eval "docker exec -it docker_php_1 /bin/bash"
+    eval "docker-compose up -d"
   elif [ "$task" = "build" ]; then
-    eval "docker"
-    fi
+    eval "docker-compose build"
+  elif [ "$task" = "down" ]; then
+    eval "docker-compose down"
+  elif [ "$task" = "run"]; then
+    eval "docker exec -it app_php /bin/bash"
  fi
